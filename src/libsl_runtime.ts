3e0115d259222627ef1e6ede1316b08701a6b7c2
Mikhail Onischuck <43421682+dog-m@users.noreply.github.com>
2024-07-07 11:59:52 +0300

    Added support implementations for helper functions + Minor build improvements

--- src/libsl_runtime.ts
+++ src/libsl_runtime.ts
@@ -6,11 +6,99 @@ interface ErrorWithCode extends Error {
     code: number;
 }
 
+// #problem: do we need this?
+interface AggregateError extends Error {
+}
+interface AggregateErrorConstructor extends ErrorConstructor {
+    new (message?: string): AggregateError;
+}
+declare var AggregateError: AggregateErrorConstructor;
 
-// note: adapted from c++ sources
-function __lsl_err_generic(clazz: string, errCode: number, msg: string): ErrorWithCode {
+
+
+// note: adapted from c++ sources, see ContainerError::BusinessError
+function __lsl_new_ERROR(clazz: string, errCode: number, msg: string): ErrorWithCode {
     let err = new Error(msg) as ErrorWithCode; // #problem: will there be a TypeError thrown here or not?
     err.code = errCode;
     err.name = clazz;
     return err;
 }
+
+
+// note: adapted from c++ sources, see ContainerError::BusinessError
+function __lsl_new_EVAL_ERROR(clazz: string, errCode: number, msg: string): ErrorWithCode {
+    let err = new EvalError(msg) as ErrorWithCode; // #problem: will there be a TypeError thrown here or not?
+    err.code = errCode;
+    err.name = clazz;
+    return err;
+}
+
+
+// note: adapted from c++ sources, see ContainerError::BusinessError
+function __lsl_new_RANGE_ERROR(clazz: string, errCode: number, msg: string): ErrorWithCode {
+    let err = new RangeError(msg) as ErrorWithCode; // #problem: will there be a TypeError thrown here or not?
+    err.code = errCode;
+    err.name = clazz;
+    return err;
+}
+
+
+// note: adapted from c++ sources, see ContainerError::BusinessError
+function __lsl_new_REFERENCE_ERROR(clazz: string, errCode: number, msg: string): ErrorWithCode {
+    let err = new ReferenceError(msg) as ErrorWithCode; // #problem: will there be a TypeError thrown here or not?
+    err.code = errCode;
+    err.name = clazz;
+    return err;
+}
+
+
+// note: adapted from c++ sources, see ContainerError::BusinessError
+function __lsl_new_TYPE_ERROR(clazz: string, errCode: number, msg: string): ErrorWithCode {
+    let err = new TypeError(msg) as ErrorWithCode; // #problem: will there be a TypeError thrown here or not?
+    err.code = errCode;
+    err.name = clazz;
+    return err;
+}
+
+
+// note: adapted from c++ sources, see ContainerError::BusinessError
+function __lsl_new_AGGREGATE_ERROR(clazz: string, errCode: number, msg: string): ErrorWithCode {
+    let err = new AggregateError(msg) as ErrorWithCode; // #problem: will there be a TypeError thrown here or not?
+    err.code = errCode;
+    err.name = clazz;
+    return err;
+}
+
+
+// note: adapted from c++ sources, see ContainerError::BusinessError
+function __lsl_new_URI_ERROR(clazz: string, errCode: number, msg: string): ErrorWithCode {
+    let err = new URIError(msg) as ErrorWithCode; // #problem: will there be a TypeError thrown here or not?
+    err.code = errCode;
+    err.name = clazz;
+    return err;
+}
+
+
+// note: adapted from c++ sources, see ContainerError::BusinessError
+function __lsl_new_SYNTAX_ERROR(clazz: string, errCode: number, msg: string): ErrorWithCode {
+    let err = new SyntaxError(msg) as ErrorWithCode; // #problem: will there be a TypeError thrown here or not?
+    err.code = errCode;
+    err.name = clazz;
+    return err;
+}
+
+
+// #question: do we actually need those?
+/*
+// note: adapted from c++ sources, see ContainerError::BusinessError
+function __lsl_new_OOM_ERROR(clazz: string, errCode: number, msg: string): ErrorWithCode {
+    ...
+}
+
+
+// note: adapted from c++ sources, see ContainerError::BusinessError
+function __lsl_new_TERMINATION_ERROR(clazz: string, errCode: number, msg: string): ErrorWithCode {
+    ...
+}
+*/
+
