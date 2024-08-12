a99eaf3d233afa3525cb1b1c489ba395350e46e6
Mikhail Onischuck <43421682+dog-m@users.noreply.github.com>
2024-08-12 23:18:44 +0300

    Added marker for hand-written files within `src` filder

--- src/libsl_runtime.ts
+++ src/libsl_runtime.ts
@@ -1,4 +1,4 @@
-// ???
+/// NOTE: this is part of a set of hand-written runtime support libraries for `typescript` language target
 
 import { Engine, SymbolicIdentityMap, SymbolicList, SymbolicMap } from "@org.usvm.api";
 
